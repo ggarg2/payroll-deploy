d9651428c5bb85446cee6ba0e89ae3f99799b2e1
Gaurav Garg <garg.gaurav.1989it@gmail.com>
2018-03-08 23:46:29 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require('express');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/[^\.]+$', function(req, res) {
+    res.set('Content-Type', 'text/html')
+        .sendFile("/dist/index.html", { "root": __dirname })
+})
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Node server listening on http://localhost:3000`);
+});
